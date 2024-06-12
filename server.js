00e09f33d29a37e1088cd92aa094e2c22955684b
Devanshu <devanshupanigrahi2004@gmail.com>
2024-06-12 22:02:55 +0530

    added import statement in cors

--- server.js
+++ server.js
@@ -2,18 +2,12 @@ const express = require('express');
 const { Web3 } = require('web3');
 const { PrismaClient } = require('@prisma/client');
 const Redis = require('ioredis');
-const cors = require('cors');
+import cors from 'cors';
 
 const app = express();
 
 // CORS configuration
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  res.header("Access-Control-Allow-Credentials", "true");
-  next();
-});
+app.use(cors());
 
 
 const prisma = new PrismaClient();
