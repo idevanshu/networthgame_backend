775a3b5d893662e1b87bacc90c292dec4f9c77ca
Devanshu <devanshupanigrahi2004@gmail.com>
2024-06-12 20:57:59 +0530

    allowed credentials too

--- server.js
+++ server.js
@@ -10,13 +10,14 @@ const app = express();
 app.use(cors({
   origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
 }));
 
 const prisma = new PrismaClient();
 const redis = new Redis();
 
-const infuraUrl = process.env.INFURA_URL || 'https://mainnet.infura.io/v3/8627168fd72846898c561bf658ff262a';
+const infuraUrl = process.env.INFURA_URL || 'https://mainnet.infura.io/v3/8627168fd72846898c561bf658ff262a'; // Replace with your Infura Project ID
 const web3 = new Web3(infuraUrl);
 
 app.use(express.urlencoded({ extended: true }));
